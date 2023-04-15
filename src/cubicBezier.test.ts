3d71b97f443dfff3a3cf336bba6cb100e7c06066
Yuki Iwanaga <yuki@creasty.com>
2023-04-15 14:15:48 +0900

    Add test case

--- src/cubicBezier.test.ts
+++ src/cubicBezier.test.ts
@@ -4,6 +4,13 @@ import { Easing } from "./easing";
 const ts = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
 
 describe("cubicBezier", () => {
+  test("emulates Easing.Linear", () => {
+    const fn = cubicBezier(0, 0, 1, 1);
+    ts.forEach((t) => {
+      expect(fn(t)).toEqual(Easing.Linear(t));
+    });
+  });
+
   test("emulates Easing.Sine.In", () => {
     const fn = cubicBezier(0.12, 0, 0.39, 0);
     ts.forEach((t) => {
